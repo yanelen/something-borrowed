d7dfb97988e1b4200433c9ee8ec86a7e6694a054
Yanelen <yanelen@yahoo.com>
2015-11-08 06:56:11 -0500

    updated initializers

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( angular.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( ie-emulation-modes-warning.js )
+Rails.application.config.assets.precompile += %w( ie10-viewport-bug-workaround.js )
