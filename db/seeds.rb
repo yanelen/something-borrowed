475ba6cb3b0a0b9f26083608e9c1af6cb9f92a1e
Casey Panzer <caseypanzer@Caseys-MacBook-Air.local>
2015-11-09 16:25:11 -0500

    Merge branch 'development' of https://github.com/sepowitz/project_three into sepowitz-development
    # Conflicts:
    #	app/views/layouts/angularlayout.html.erb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,44 +6,44 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-users = User.create([{email: "casey.panzer@gmail.com", username: "cpanzer", password: 123456},{email: "s@gmail.com", username: "sodell", password: 123456}])
+users = User.create([{email: "@gmail.com", username: "cpanzer", password: "123456"},{email: "s@gmail.com", username: "sodell", password: "123456"}])
 
 posts = Post.create(
 [
 	{
 		user_id: 1,
-		borrower_id: null,
+		borrower_id: nil,
 		title: 'bicycle',
 		description: 'brand new, 12-speed',
-    latitude: 40.738698,
-    longitude: -73.993250,
-		available: true
+    latitude: "40.738698",
+    longitude: "-73.993250",
+		available: true,
 	},
 	{
 		user_id: 2,
 		borrower_id: 1,
 		title: 'catapult',
 		description: 'can hit Russia from Alaska',
-    latitude: 40.738668,
-    longitude: -73.993250,
-		available: false
-	},
-	{
-		user_id: 3,
-		borrower_id: 1,
-		title: 'iron',
-		description: 'works great on t-shirts',
-    latitude: 40.738888,
-    longitude: -73.993250,
+    latitude: "40.738668",
+    longitude: "-73.993250",
 		available: false
 	},
 	{
 		user_id: 1,
-		borrower_id: null,
+		borrower_id: 2,
+		title: 'iron',
+		description: 'works great on t-shirts',
+    latitude: "40.738888",
+    longitude: "-73.993250",
+		available: false
+	},
+	{
+		user_id: 1,
+		borrower_id: nil,
 		title: 'ladder',
 		description: '25-foot ladder. Works great for saving cats.',
-    latitude: 40.738698,
-    longitude: -73.994250,
+    latitude: "40.738698",
+    longitude: "-73.994250",
 		available: true
 	},
 	{
@@ -51,26 +51,26 @@ posts = Post.create(
 		borrower_id: 2,
 		title: 'bicycle',
 		description: 'brand new, 12-speed',
-    latitude: 40.738788,
-    longitude: -73.993240,
+    latitude: "40.738788",
+    longitude: "-73.993240",
 		available: false
 	},
 	{
 		user_id: 2,
-		borrower_id: null,
+		borrower_id: nil,
 		title: 'fan',
 		description: 'you know, like a paper fan for you face or whatever',
-    latitude: 40.738668,
-    longitude: -73.993280,
+    latitude: "40.738668",
+    longitude: "-73.993280",
 		available: true
 	},
 	{
-		user_id: 3,
-		borrower_id: null,
+		user_id: 1,
+		borrower_id: nil,
 		title: 'air rifle',
 		description: 'great for getting rid of pesty critters',
-    latitude: 40.739688,
-    longitude: -73.993270,
+    latitude: "40.739688",
+    longitude: "-73.993270",
 		available: true
 	}
 ]);
