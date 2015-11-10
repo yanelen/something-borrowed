d49e8e650f5ffe6c5cccf6debf08bfba16b3fe88
Sebastian Odell <sebastianodell@gmail.com>
2015-11-10 09:24:07 -0500

    angular filters a go

--- app/assets/javascripts/angular.js
+++ app/assets/javascripts/angular.js
@@ -38,64 +38,16 @@ app.controller('MainController', ['$http', function($http){
 app.controller('ItemController', ['$http', function($http){
 	var itemCtrl = this;
 
-	$http.get('/posts').success(function(data){
+	itemCtrl.getItems = function(){$http.get('/posts').success(function(data){
 		itemCtrl.itemList = data.posts
 		console.log(itemCtrl.itemList)
 	})
+}
+
+itemCtrl.getItems()
+
+
 
 }]);
-
-//This is a placeholder item info while we wait for routes and JSON
-// var items = [
-// 	{
-// 		user_id: 1,
-// 		borrower_id: null,
-// 		title: 'bicycle',
-// 		description: 'brand new, 12-speed',
-// 		available: true
-// 	},
-// 	{
-// 		user_id: 2,
-// 		borrower_id: 1,
-// 		title: 'catapult',
-// 		description: 'can hit Russia from Alaska',
-// 		available: false
-// 	},
-// 	{
-// 		user_id: 3,
-// 		borrower_id: 1,
-// 		title: 'iron',
-// 		description: 'works great on t-shirts',
-// 		available: false
-// 	},
-// 	{
-// 		user_id: 1,
-// 		borrower_id: null,
-// 		title: 'ladder',
-// 		description: '25-foot ladder. Works great for saving cats.',
-// 		available: true
-// 	},
-// 	{
-// 		user_id: 1,
-// 		borrower_id: 2,
-// 		title: 'bicycle',
-// 		description: 'brand new, 12-speed',
-// 		available: false
-// 	},
-// 	{
-// 		user_id: 2,
-// 		borrower_id: null,
-// 		title: 'fan',
-// 		description: 'you know, like a paper fan for you face or whatever',
-// 		available: true
-// 	},
-// 	{
-// 		user_id: 3,
-// 		borrower_id: null,
-// 		title: 'air rifle',
-// 		description: 'great for getting rid of pesty critters',
-// 		available: true
-// 	}
-// ];
 
 })();
