ccb89c5c827bb3cb3655e0c0a6b13da233d7832b
Edwin Choi <edwinc91@Edwin-Chois-MBP.home>
2015-11-11 14:55:28 -0500

    forgot to comment and uncomment out things

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,8 @@
 class PostsController < ApplicationController
 
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
 
-  # before_action :require_current_user
+  before_action :require_current_user
 
   def testangular
     render '/testangular', layout: "angularlayout"
