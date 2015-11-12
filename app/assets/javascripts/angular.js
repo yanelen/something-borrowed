42f3801dfcfed0174328ed8240c47dc831cb2474
Casey Panzer <caseypanzer@Caseys-MacBook-Air.local>
2015-11-12 16:33:30 -0500

    comments for borrower work

--- app/assets/javascripts/angular.js
+++ app/assets/javascripts/angular.js
@@ -123,22 +123,29 @@ itemCtrl.returnItem = function(item){
 });
 };
 
-itemCtrl.createComment = function(post_id, user_id){
 
-	$http.post('/posts/'+post_id+'/comments', {
-		authenticity_token: authenticity_token,
-		comment: {
-			post_id: post_id,
-			comment: this.newComment,
-			user_id: user_id
-		}
-	}).success(function(data){
-		console.log(data);
-		itemCtrl.getItems();
-	});
 
-};
 
+}]);
+
+app.controller('CommentsController', ['$http', '$scope', function($http, $scope){
+
+	this.createComment = function(){
+			var authenticity_token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+		console.log($scope);
+		$http.post('/posts/'+$scope.$parent.item.id+'/comments', {
+			authenticity_token: authenticity_token,
+			comment: {
+				post_id: $scope.$parent.item.id,
+				comment: this.newComment,
+				user_id: $scope.$parent.item.user_id,
+				user_name: $scope.$parent.item.id.user_name
+			}
+		}).success(function(data){
+			console.log(data);
+			$scope.$parent.$parent.itemCtrl.getItems();
+		});
+	};
 
 }]);
 
